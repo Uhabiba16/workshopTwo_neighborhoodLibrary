5134ccd5de9a9807b9adfc8982ce75de688c6959
Umme Habiba <humme.career@gmail.com>
2025-04-20 13:39:16 -0400

    Avaiable Book list from menu

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -3,24 +3,56 @@ package com.ps;
 import java.util.Scanner;
 
 public class Main {
+    public static void availableBooks(Book[] bookList) {
+        System.out.println("Available Books");
+        for (Book book : bookList) {
+            if (book != null && !book.isCheckedOut()) {
+                System.out.println("Book ID: " + book.getId() + " Book Title: " + book.getTitle() + " Book ISBN: " + book.getIsbn());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Book[] bookList = new Book[20];
+        bookList[0] = new Book(1, "123-00001", "Harry Potter1", false, "");
+        bookList[1] = new Book(2, "123-00002", "Harry Potter2", false, "");
+        bookList[2] = new Book(3, "123-00003", "Harry Potter3", false, "");
+        bookList[3] = new Book(4, "123-00004", "Harry Potter4", false, "");
+        bookList[4] = new Book(5, "123-00004", "Harry Potter5", true, "");
+        bookList[5] = new Book(6, "123-00005", "Harry Potter6", false, "");
+        bookList[6] = new Book(7, "123-00007", "Harry Potter7", false, "");
+        bookList[7] = new Book(8, "123-00008", "Game of Thrones1", false, "");
+        bookList[8] = new Book(9, "123-00009", "Game of Thrones2", false, "");
+        bookList[9] = new Book(10, "123-000010", "Game of Thrones3", false, "");
+        bookList[10] = new Book(11, "123-000011", "Game of Thrones4", true, "");
+        bookList[11] = new Book(12, "123-000012", "Game of Thrones5", false, "");
+        bookList[12] = new Book(13, "123-000013", "The Lord of the Rings1", false, "");
+        bookList[13] = new Book(14, "123-000014", "The Lord of the Rings2", false, "");
+        bookList[14] = new Book(15, "123-000015", "The Lord of the Rings3", false, "");
+        bookList[15] = new Book(16, "123-000016", "Java for Dummies", false, "");
+        bookList[16] = new Book(17, "123-000017", "Python for Dummies", true, "");
+        bookList[17] = new Book(18, "123-000018", "How to be a Software Engineer", false, "");
+        bookList[18] = new Book(19, "123-000019", "Web Design 101", false, "");
+        bookList[19] = new Book(20, "123-000020", "Artificial Intelligence", true, "");
+
         boolean mainMenu = true;
         while (mainMenu) {
-
             System.out.println("\nWelcome to Neighborhood Library!");
             System.out.println("\nShow Available Books option \"1\"");
             System.out.println("Show Checked Out Books option \"2\"");
             System.out.println("Exit option \"0\"");
-            System.out.print("\n Enter option: ");
+            System.out.print("\nEnter option: ");
             int option = scanner.nextInt();
+            scanner.nextLine();
 
             switch (option) {
                 case 1:
-                    // method for available books
+                    availableBooks(bookList);
                     break;
                 case 2:
-                    // method for checked out books
+                    // checked out books method
                     break;
                 case 0:
                     mainMenu = false;
@@ -30,30 +62,5 @@ public class Main {
                     System.out.println("\nPlease enter \"1\", \"2\", or \"0\" for menu options");
             }
         }
-        Book[] bookList = new Book[20];
-
-        bookList[0] = new Book(1, "123-00001", "Harry Potter1", false, "");
-        bookList[1] = new Book(2, "123-00002", "Harry Potter2", false, "");
-        bookList[2] = new Book(3, "123-00003", "Harry Potter3", false, "");
-        bookList[3] = new Book(4, "123-00004", "Harry Potter4", false, "");
-        bookList[4] = new Book(5, "123-00004", "Harry Potter5", true, "");
-        bookList[5] = new Book(6, "123-00005", "Harry Potter6", false, "");
-        bookList[6] = new Book(7, "123-00007", "Harry Potter7", true, "");
-        bookList[7] = new Book(8, "123-00008", "Game of Thrones1", false, "");
-        bookList[8] = new Book(9, "123-00009", "Game of Thrones2", true, "");
-        bookList[9] = new Book(10, "123-000010", "Game of Thrones3", false, "");
-        bookList[10] = new Book(11, "123-000011", "Game of Thrones4", true, "");
-        bookList[11] = new Book(12, "123-000012", "Game of Thrones5", false, "");
-        bookList[12] = new Book(13, "123-000013", "The Lord of the Rings1", false, "");
-        bookList[13] = new Book(14, "123-000014", "The Lord of the Rings2", false, "");
-        bookList[14] = new Book(15, "123-000015", "The Lord of the Rings3", false, "");
-        bookList[15] = new Book(16, "123-000016", "Java for Dummies", true, "");
-        bookList[16] = new Book(17, "123-000017", "Python for Dummies", true, "");
-        bookList[17] = new Book(18, "123-000018", "How to be a Software Engineer", false, "");
-        bookList[18] = new Book(19, "123-000019", "Web Design 101", false, "");
-        bookList[19] = new Book(20, "123-000020", "Artificial Intelligence", true, "");
-
     }
-
-
 }
